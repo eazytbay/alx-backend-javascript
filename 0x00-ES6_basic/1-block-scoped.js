c92e405561de41733a6e01476c8fa8912e2979fb
eazytbay <eazytbay@gmail.com>
2024-06-26 17:23:37 +0000

    error fixed

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
-  if (trueOrFalse) {
-    return [true, false];
-  } else {
-    return [task, task2]; 
-  }
+
+  // assignment Destructuring and conditional logic
+  const [newTask, newTask2] = trueOrFalse ? [true, false] : [task, task2];
+  return [newTask, newTask2];
 }
+
 
 
