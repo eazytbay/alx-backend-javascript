92e1a8791bb45ecdb05ce7eec1ab1e21385ef321
eazytbay <eazytbay@gmail.com>
2024-06-27 15:51:20 +0000

    100-evcar.js fixed

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -5,9 +5,13 @@ export default class EVCar extends Car {
 	constructor(brand, motor, color, range) {
 		super(brand, motor, color);
 		this._range = range;
+	static get [Symbol.species](){
+	  return this;
+	}
 	}
 
 	cloneCar() {
-		return new Car();
+	  const cloneCar = this.constructor[Symbol.species]();
+		return new cloneCar;
 	}
 }
