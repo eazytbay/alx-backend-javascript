bac3397c4fd59b61664b0424bd87549dfe082152
eazytbay <eazytbay@gmail.com>
2024-06-27 16:27:44 +0000

    fixing errors

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,23 +1,20 @@
-/* eslint-disable  no-underscore-dangle */
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = floors;
+    this.floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
-  set floors(x) {
-    if (typeof x !== 'number') throw new TypeError();
-    this._floors = x;
+  set floors(value) {
+    this._floors = value;
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
-	
