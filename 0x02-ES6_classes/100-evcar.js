e047d3eb741910118b8a1137f3de943fa2e64a83
eazytbay <eazytbay@gmail.com>
2024-06-27 16:14:21 +0000

    100-evcar.js updated

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,26 +1,14 @@
-import Car from './10-car'; 
+import Car from './10-car.js';
 
-export default class EVCar extends Car {
+class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    super(brand, motor, color); 
+    super(brand, motor, color);
     this._range = range;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  get range() {
-    return this._range;
   }
 
   cloneCar() {
     return new Car();
   }
-
-  toString() {
-    return `EVCar { _brand: '${this._brand}', _motor: '${this._motor}', _color: '${this._color}', _range: '${this._range}' }`;
-  }
 }
 
-
+export default EVCar;
