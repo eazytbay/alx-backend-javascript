958ecc40f970721a0cac6fa10da36f1d367eea26
eazytbay <eazytbay@gmail.com>
2024-07-02 00:42:41 +0100

    fixing errors

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
+  }
   const customBuffer = new ArrayBuffer(length);
-  const x = new DataView(buffer);
-
-  // check if position outside range
-  if (position >= length) throw new Error('Position outside range');
-  // set the new value in the desired position
-  x.setInt8(position, value);
-
-  return x;
+  const int8 = new Int8Array(customBuffer, 0, length);
+  int8.set([value], position);
+  return new DataView(customBuffer);
 }
