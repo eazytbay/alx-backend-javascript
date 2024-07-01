94ef4df40ca946eea8357bf3af445a3e03aa12e8
eazytbay <eazytbay@gmail.com>
2024-07-02 00:26:46 +0100

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,11 @@
-export default function cleanSet(set, startString) {
-  let wrd = '';
-  const array = [];
-
-  if (startString && typeof startString === 'string') {
-    for (const element of set) {
-      if (element && element.startsWith(startString)) {
-        array.push(element.slice(startString.length));
-      }
-    }
-    wrd = array.join('-');
+const cleanSet = (set, startString) => {
+  if (startString === undefined || startString.length === 0) {
+    return '';
   }
-  return wrd;
-}
+  return [...set]
+    .filter((parametro) => (parametro !== undefined ? parametro.startsWith(startString) : ''))
+    .map((parametro) => (parametro !== undefined ? parametro.slice(startString.length) : ''))
+    .join('-');
+};
+
+export default cleanSet;
