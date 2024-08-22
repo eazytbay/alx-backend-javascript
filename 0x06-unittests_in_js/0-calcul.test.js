e877ae25d5c379ad09809a1b19cc7658aa302389
eazytbay <eazytbay@gmail.com>
2024-08-22 22:25:45 +0100

    task 0 minor fix

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,10 +1,31 @@
-const assert = require('assert');
-const calculateNumber = require("./0-calcul.js");
-describe('Testing addition of rounded off numbers', function() {
-	it('should return correct output', function() {
-		assert.strictEqual(calculateNumber(1, 3), 4);
-		assert.strictEqual(calculateNumber(1, 3.7), 5);
-		assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-		assert.strictEqual(calculateNumber(0, 0), 0);
-	});
-	});
+const assert = require("assert");
+const { it, describe } = require("mocha");
+const calculateNumber = require("./0-calcul");
+
+describe("calculateNumber()", function() {
+
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1, 2);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.4, 2.2);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.6, 2.7);
+      assert.strictEqual(res, 5);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(0, 0);
+      assert.strictEqual(res, 0);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.6, -1.7);
+      assert.strictEqual(res, -4);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.4, -1.3);
+	assert.strictEqual(res, -2);
+    });
+});
